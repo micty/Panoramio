4ddfccd5ffd4829c0ef9b44527353b8d9714bb39
unknown <micty_deng@kingdee.gbl>
2016-10-25 17:53:20 +0800

    config

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ module.exports = {
         'userId': '841653',
         'userId': '2732962',
         'userId': '5167299',
-        'userId': '1635631',
+        'userId': '4818033',
         'pageNos': '1-',
     },
 
     '/Tasks': [
-        'user.avatar',
-        'user.comments',
-        'user.fav-users',
-        'user.groups',
+        //'user.avatar',
+        //'user.comments',
+        //'user.fav-users',
+        //'user.groups',
 
         'photo.detail',
         'photo.stats',
@@ -26,21 +26,21 @@ module.exports = {
 
 
     '/User': {
-        url: 'https://ssl.panoramio.com/user/{id}',
+        url: 'https://ssl.panoramio.com/user/{id}?show=all',
         cache: true,
         html: {
-            file: '../data-1635631/{id}/html/index.html',
+            file: '../data-4818033/{id}/html/index.html',
             write: true,
         },
         json: {
-            file: '../data-1635631/{id}/json/index.json',
+            file: '../data-4818033/{id}/json/index.json',
             write: true,
         },
     },
 
     '/Avatar': {
         cache: true,
-        file: '../data-1635631/{userId}/avatar.jpg',
+        file: '../data-4818033/{userId}/avatar.jpg',
         write: true,
     },
     
@@ -48,11 +48,11 @@ module.exports = {
         url: 'https://ssl.panoramio.com/user/{userId}?comment_page={no}',
         cache: true,
         html: {
-            file: '../data-1635631/{userId}/html/comments/{sn}.html',
+            file: '../data-4818033/{userId}/html/comments/{sn}.html',
             write: true,
         },
         json: {
-            file: '../data-1635631/{userId}/json/comments/{sn}.json',
+            file: '../data-4818033/{userId}/json/comments/{sn}.json',
             write: true,
         },
     },
@@ -60,11 +60,11 @@ module.exports = {
     '/FavUsers': {
         cache: true,
         html: {
-            file: '../data-1635631/{userId}/html/fav-users.html',
+            file: '../data-4818033/{userId}/html/fav-users.html',
             write: true,
         },
         json: {
-            file: '../data-1635631/{userId}/json/fav-users.json',
+            file: '../data-4818033/{userId}/json/fav-users.json',
             write: true,
         },
     },
@@ -72,11 +72,11 @@ module.exports = {
      '/Groups': {
         cache: true,
         html: {
-            file: '../data-1635631/{userId}/html/groups.html',
+            file: '../data-4818033/{userId}/html/groups.html',
             write: true,
         },
         json: {
-            file: '../data-1635631/{userId}/json/groups.json',
+            file: '../data-4818033/{userId}/json/groups.json',
             write: true,
         },
     },
@@ -86,11 +86,11 @@ module.exports = {
         url: 'https://ssl.panoramio.com/favorites/{userId}',
         cache: true,
         html: {
-            file: '../data-1635631/{userId}/html/favorites.html',
+            file: '../data-4818033/{userId}/html/favorites.html',
             write: true,
         },
         json: {
-            file: '../data-1635631/{userId}/json/favorites.json',
+            file: '../data-4818033/{userId}/json/favorites.json',
             write: true,
         },
     },
@@ -100,25 +100,25 @@ module.exports = {
         url: 'https://ssl.panoramio.com/favorites/{userId}?photo_page={no}',
         cache: true,
         html: {
-            file: '../data-1635631/{userId}/html/fav-photos/{sn}.html',
+            file: '../data-4818033/{userId}/html/fav-photos/{sn}.html',
             write: true,
         },
         json: {
-            file: '../data-1635631/{userId}/json/fav-photos/{sn}.json',
+            file: '../data-4818033/{userId}/json/fav-photos/{sn}.json',
             write: true,
         },
     },
 
 
     '/Page': {
-        url: 'https://ssl.panoramio.com/user/{userId}?photo_page={no}',
+        url: 'https://ssl.panoramio.com/user/{userId}?show=all&photo_page={no}',
         cache: true,
         html: {
-            file: '../data-1635631/{userId}/html/page/{sn}.html',
+            file: '../data-4818033/{userId}/html/page/{sn}.html',
             write: true,
         },
         json: {
-            file: '../data-1635631/{userId}/json/page/{sn}.json',
+            file: '../data-4818033/{userId}/json/page/{sn}.json',
             write: true,
         },
     },
@@ -127,11 +127,11 @@ module.exports = {
         url: 'https://ssl.panoramio.com/photo/{id}',
         cache: true,
         html: {
-            file: '../data-1635631/global/html/photo/{id}.html',
+            file: '../data-4818033/global/html/photo/{id}.html',
             write: true,
         },
         json: {
-            file: '../data-1635631/global/json/photo/{id}.json',
+            file: '../data-4818033/global/json/photo/{id}.json',
             write: true,
         },
     },
@@ -140,11 +140,11 @@ module.exports = {
         url: 'https://ssl.panoramio.com/photo/{id}/stats',
         cache: true,
         html: {
-            file: '../data-1635631/global/html/stats/{id}.html',
+            file: '../data-4818033/global/html/stats/{id}.html',
             write: true,
         },
         json: {
-            file: '../data-1635631/global/json/stats/{id}.json',
+            file: '../data-4818033/global/json/stats/{id}.json',
             write: true,
         },
     },
@@ -156,19 +156,19 @@ module.exports = {
 
         thumbnail: {
             url: 'https://mw2.google.com/mw-panoramio/photos/thumbnail/{id}.jpg',
-            file: '../data-1635631/global/image/thumbnail/{id}.jpg',
+            file: '../data-4818033/global/image/thumbnail/{id}.jpg',
         },
         medium: {
             url: 'https://mw2.google.com/mw-panoramio/photos/medium/{id}.jpg',
-            file: '../data-1635631/global/image/medium/{id}.jpg',
+            file: '../data-4818033/global/image/medium/{id}.jpg',
         },
         large: {
             url: 'https://static.panoramio.com.storage.googleapis.com/photos/large/{id}.jpg',
-            file: '../data-1635631/global/image/large/{id}.jpg',
+            file: '../data-4818033/global/image/large/{id}.jpg',
         },
         origin: {
             url: 'https://ssl.panoramio.com/photos/d/{id}.jpg',
-            file: '../data-1635631/global/image/origin/{id}.jpg',
+            file: '../data-4818033/global/image/origin/{id}.jpg',
         },
       
     },
